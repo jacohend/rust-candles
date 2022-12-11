99cd8e48ce294f942b5a49c08e9dc44a5d72b1e9
jacohend <jacob@tierion.com>
2022-12-10 23:19:04 -0500

    use flisky's tui renderer

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use std::error::Error;
-use std::{thread, time::Duration};
+use std::{process, thread, time::Duration};
 use cli_candlestick_chart::{Candle, Chart};
 use serde::{Serialize, Deserialize};
+use tui::{widgets::Widget, style::Modifier};
+use unicode_width::UnicodeWidthStr;
+use ansi_parser::AnsiParser;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 struct BinanceKlinesItem {
@@ -19,10 +22,67 @@ struct BinanceKlinesItem {
     ignore: String,
 }
 
+pub struct AnsiEscape<'a>(&'a str);
+
+impl<'a> Widget for AnsiEscape<'a> {
+    fn render(self, area: tui::layout::Rect, buf: &mut tui::buffer::Buffer) {
+        for (h, line) in self.0.lines().enumerate() {
+            let h = area.top() + h as u16;
+            let mut w = area.left();
+            let mut s = tui::style::Style::default();
+            for block in line.ansi_parse() {
+                match block {
+                    ansi_parser::Output::TextBlock(text) => {
+                        if w < buf.area.width {
+                            buf.set_string(w, h, text, s);
+                            w += text.width() as u16;
+                        }
+                    }
+                    ansi_parser::Output::Escape(escape) => match escape {
+                        ansi_parser::AnsiSequence::SetGraphicsMode(v) => {
+                            fn color(v: &[u8]) -> tui::style::Color {
+                                match v[1] {
+                                    2 => tui::style::Color::Rgb(v[2], v[3], v[4]),
+                                    5 => tui::style::Color::Indexed(v[2]),
+                                    _ => panic!("unsupport color"),
+                                }
+                            }
+
+                            s = match v[0] {
+                                0 => tui::style::Style::default(),
+                                1 => s.add_modifier(Modifier::BOLD),
+                                2 => s.remove_modifier(Modifier::BOLD),
+                                38 => tui::style::Style::default().fg(color(&v)),
+                                48 => tui::style::Style::default().bg(color(&v)),
+                                v => panic!("unsupport attribute: {v}"),
+                            };
+                        }
+                        ansi_parser::AnsiSequence::ResetMode(_) => {
+                            s = tui::style::Style::default();
+                        }
+                        _ => panic!("unssport escape sequence"),
+                    },
+                }
+            }
+        }
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
+
+    ctrlc::set_handler(move || {
+        println!("received Ctrl+C!");
+        process::exit(0x0100);
+    }).expect("Error setting Ctrl-C handler");
+
+    crossterm::terminal::enable_raw_mode()?;
+    let mut stdout = std::io::stdout();
+    crossterm::execute!(stdout, crossterm::terminal::EnterAlternateScreen)?;
+    let mut terminal = tui::Terminal::new(tui::backend::CrosstermBackend::new(stdout))?;
+
     loop {
         let candles =
-            reqwest::blocking::get("https://api.binance.us/api/v1/klines?symbol=BTCUSDT&interval=1m")?
+            reqwest::blocking::get("https://api.binance.us/api/v1/klines?symbol=BTCUSDT&interval=15m")?
                 .json::<Vec<BinanceKlinesItem>>()?
                 .iter()
                 .map(|candle| {
@@ -47,8 +107,13 @@ fn main() -> Result<(), Box<dyn Error>> {
         chart.set_volume_pane_height(4);
         chart.set_volume_pane_enabled(true);
         // chart.set_volume_pane_unicode_fill(true);
-        print!("{}[2J", 27 as char); //clear terminal for new draw
-        chart.draw();
+        terminal.draw(|frame| {
+            let area = frame.size().inner(&tui::layout::Margin {
+                vertical: 2,
+                horizontal: 2,
+            });
+            frame.render_widget(AnsiEscape(&chart.render()), area);
+        })?;
         thread::sleep(Duration::from_millis(15000));
     }
     Ok(())
